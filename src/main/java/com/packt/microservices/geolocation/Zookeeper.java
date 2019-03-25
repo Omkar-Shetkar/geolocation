b7fca97bead135de44155a191efedf313838a74f
Omkar Shetkar <omkar.shetkar@outlook.com>
2019-03-25 10:47:38 +0530

    Zookeeper register logic added.

--- src/main/java/com/packt/microservices/geolocation/Zookeeper.java
+++ src/main/java/com/packt/microservices/geolocation/Zookeeper.java
@@ -3,6 +3,14 @@ package com.packt.microservices.geolocation;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+import org.apache.curator.x.discovery.ServiceDiscovery;
+import org.apache.curator.x.discovery.ServiceDiscoveryBuilder;
+import org.apache.curator.x.discovery.ServiceInstance;
+import org.apache.curator.x.discovery.UriSpec;
+
 public class Zookeeper {
 
 	private String host;
@@ -35,6 +43,30 @@ public class Zookeeper {
 	}
 
 	public void register() {
+		CuratorFramework curator = CuratorFrameworkFactory.newClient(host + ":" + port, new RetryNTimes(3, 3000));
+		curator.start();
 
+		try {
+			final ServiceInstance<Object> serviceInstance = ServiceInstance.builder()
+					.uriSpec(new UriSpec("{scheme}://{address}:{port}")).address(getIp()).port(getPort())
+					.name("geolocation").build();
+
+			final ServiceDiscovery<Object> serviceDiscovery = ServiceDiscoveryBuilder.builder(Object.class)
+					.basePath("com.packt.microservices").client(curator).thisInstance(serviceInstance).build();
+
+			serviceDiscovery.start();
+
+			Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+				try {
+					serviceDiscovery.unregisterService(serviceInstance);
+				} catch (Exception e) {
+					System.err.println("Error while unregistering service in Zookeeper. Details: " + e.getMessage());
+					e.printStackTrace();
+				}
+			}));
+		} catch (Exception e) {
+			System.err.println("Error while registering service in Zookeeper. Details: " + e.getMessage());
+			e.printStackTrace();
+		}
 	}
 }
